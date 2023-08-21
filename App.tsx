f1dd989901418309230da17bd9a98165b695d271
Mateus Oliveira <mateussiiloliveira@gmail.com>
2023-08-21 19:38:02 -0300

    refactor: changed NavigationContainer

--- App.tsx
+++ App.tsx
@@ -4,15 +4,18 @@ import { RootSiblingParent } from 'react-native-root-siblings';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Navigation from './src/routes';
+import { NavigationContainer } from '@react-navigation/native';
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RootSiblingParent> 
-        <Navigation />
-      </RootSiblingParent>
-    </QueryClientProvider>
+    <NavigationContainer>
+      <QueryClientProvider client={queryClient}>
+        <RootSiblingParent> 
+          <Navigation />
+        </RootSiblingParent>
+      </QueryClientProvider>
+    </NavigationContainer>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TimelineScreen from '../pages/TimelineScreen';
 import { MapScreen } from '../pages/MapScreen';
@@ -9,13 +8,11 @@ const Tab = createBottomTabNavigator();
 
 const Navigation = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Mapa" component={MapScreen} />
-        <Tab.Screen name="New Item" component={CreateBo} />
-        <Tab.Screen name="Timeline" component={TimelineScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Mapa" component={MapScreen} />
+      <Tab.Screen name="New Item" component={CreateBo} />
+      <Tab.Screen name="Timeline" component={TimelineScreen} />
+    </Tab.Navigator>
   );
 };
 
