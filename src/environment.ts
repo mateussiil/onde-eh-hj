0435eafb81343ec4305e9b4436d3d9c1028c1516
Mateus Oliveira <mateussiiloliveira@gmail.com>
2025-03-25 21:14:14 -0300

    fix

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
   backendURL:
-    process.env.BACKEND_URL ||
-    'https://clubesdeleitura.online/ondeEhj' ||
-    'http://10.0.0.105:3001',
+    process.env.BACKEND_URL
 };
